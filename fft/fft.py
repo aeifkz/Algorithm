9e6143ae94efe9ca0346e47393c4798981bb26e1
aeifkz <aeifkz@yahoo.com.tw>
2016-03-07 15:11:37 +0800

    change basis,and add idft

--- fft/fft.py
+++ fft/fft.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as sw
-import numpy
+import numpy as np
+import cmath
 import math
 
 N = 128
-f = 3
+f1 = 3
+f2 = 9
 
 # do inner product to compute diff
 def corr(a,b) :
@@ -16,17 +18,30 @@ def corr(a,b) :
 
 def dft(x) :
     N = len(x)
-    X = [0 for m in range(N)]
+    #X = [0 for m in range(N)]
+    X = np.zeros(N,dtype=complex)
     for m in range(N) :
-        basis = [math.cos(2*math.pi*m/N*n) for n in range(N)]		
+        #basis = np.array([math.cos(2*math.pi*m/N*n) for n in range(N)])
+        #better basis
+        basis = np.array([ cmath.exp(-1j*2*math.pi*m/N*n) for n in range(N) ])
         X[m] = corr(x,basis)
     return X
 
+def idft(X) :
+    N = len(X)
+    x = np.array([ X.dot( np.array( [cmath.exp(1j*2*math.pi*n/N*m) 
+                for m in range(N)  ]  ) ) for n in  range(N) ])
+    return x
 
-sounds = [ math.cos(2*math.pi*f/N*n) for n in range(N) ]
+
+sounds = np.array([ math.cos(2*math.pi*f1/N*n) + math.cos(2*math.pi*f2/N*n) for n in range(5,N+5) ])
+
 #fs , sounds = sw.read('/home/aeifkz/viola.wav') 
 
 
-r = dft(sounds[:])
-plt.plot(r)
+r = dft(sounds)
+s = idft(r)
+#plt.plot(r.real)
+#plt.plot(r.imag)
+plt.plot(sounds)
 plt.show()
