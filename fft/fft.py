b38d6bad8f1c985798f7a1c2fcf8eb99bae44cb3
aeifkz <aeifkz@yahoo.com.tw>
2016-03-14 16:57:42 +0800

    add FFT and iFFT

--- fft/fft.py
+++ fft/fft.py
@@ -27,6 +27,23 @@ def dft(x) :
         X[m] = corr(x,basis)
     return X
 
+def fft(x) :
+    N = len(x)
+    X = np.zeros(N,dtype=x.dtype)
+    half_N = N//2
+
+    if N==1 :
+        X[0] = x[0]
+    else :
+        x_even = np.array( [ x[2*k] for k in range(half_N) ] )
+        x_odd = np.array( [ x[2*l+1] for l in range(half_N) ] )
+        X_even = fft(x_even)
+        X_odd = fft(x_odd)
+        for m in range(N) :
+            X[m] = X_even[m % half_N] + cmath.exp(-1j*2*math.pi/N*m)*X_odd[m % half_N]
+    return X
+
+
 def idft(X) :
     N = len(X)
     x = np.array([ X.dot( np.array( [cmath.exp(1j*2*math.pi*n/N*m) 
@@ -34,14 +51,39 @@ def idft(X) :
     return x
 
 
-sounds = np.array([ math.cos(2*math.pi*f1/N*n) + math.cos(2*math.pi*f2/N*n) for n in range(5,N+5) ])
+def ifft(X) :
+    N = len(X)
+    x = np.zeros(N,dtype=X.dtype)
+    half_N = N//2
+
+    if N==1 :
+        x[0] = X[0]
+    else :
+        X_even = np.array( [ X[2*k] for k in range(half_N) ] )
+        X_odd = np.array( [ X[2*l+1] for l in range(half_N) ] )
+        x_even = ifft(X_even)
+        x_odd = ifft(X_odd)
+        for n in range(N) :
+            x[n] = x_even[n % half_N] + cmath.exp(1j*2*math.pi/N*n)*x_odd[n % half_N]
+    return x
+   
+
+
+#sounds = np.array([ math.cos(2*math.pi*f1/N*n) + math.cos(2*math.pi*f2/N*n) for n in range(0,N+0) ])
+
+phase = 0*math.pi
+
+sounds = np.array([ cmath.exp(1j*2*math.pi*f1/N*n + phase)   for n in range(N) ])
 
 #fs , sounds = sw.read('/home/aeifkz/viola.wav') 
 
 
-r = dft(sounds)
-s = idft(r)
+#r = dft(sounds)
+r = fft(sounds)
+plt.plot(sounds)
+#s = idft(r)
+s = ifft(r) 
 #plt.plot(r.real)
 #plt.plot(r.imag)
-plt.plot(sounds)
+plt.plot(s)
 plt.show()
